899426679ecb1d492b0938b4d0fc8413ed5af614
AxAxHxAxAx <126902892+AxAxHxAxAx@users.noreply.github.com>
2024-05-17 22:38:25 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,13 +6,17 @@ resource "aws_ecs_cluster" "ecs_cluster" {
   name = "my-ecs-cluster"
 }
 
+data "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+}
+
 resource "aws_ecs_task_definition" "ecs_task_definition" {
   family                   = "my-ecs-task"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([{
     name      = "my-node-app"
@@ -27,6 +31,7 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
   }])
 }
 
+
 resource "aws_ecs_service" "ecs_service" {
   name            = "my-ecs-service"
   cluster         = aws_ecs_cluster.ecs_cluster.id
@@ -39,28 +44,4 @@ resource "aws_ecs_service" "ecs_service" {
     security_groups = ["sg-0561a79cc8203131b"]      # Replace with your security group ID
     assign_public_ip = true
   }
-}
-
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
-
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
-  ]
-}
-
-variable "docker_image" {
-  description = "Docker image name"
-  type        = string
 }
