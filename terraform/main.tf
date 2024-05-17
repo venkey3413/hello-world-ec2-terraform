ba41f22a9fa45de3565855877d022fe2ed98442f
AxAxHxAxAx <126902892+AxAxHxAxAx@users.noreply.github.com>
2024-05-17 19:12:07 +0530

    Update

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,29 +8,45 @@ resource "aws_ecs_cluster" "cluster" {
 
 variable "image_url" {}
 
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "ecs-task-execution-role"
+  assume_role_policy = jsonencode({
+    "Version" => "2012-10-17",
+    "Statement" => [{
+      "Effect"    => "Allow",
+      "Principal" => {
+        "Service" => "ecs-tasks.amazonaws.com"
+      },
+      "Action"    => "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_ecs_task_definition" "task_definition" {
   family                   = "hello-world-nodejs-task"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
-
-  container_definitions = jsonencode([
-    {
-      name      = "hello-world-nodejs-container"
-      image     = var.image_url
-      cpu       = 256
-      memory    = 512
-      essential = true
-      portMappings = [
-        {
-          containerPort = 3000
-          hostPort      = 3000
-        }
-      ]
-    }
-  ])
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  container_definitions    = <<DEFINITION
+[
+  {
+    "name": "hello-world-nodejs",
+    "image": "${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}",
+    "essential": true,
+    "portMappings": [
+      {
+        "containerPort": 3000,
+        "hostPort": 3000
+      }
+    ]
+  }
+]
+DEFINITION
 }
+
 
 resource "aws_ecs_service" "service" {
   name            = "hello-world-nodejs-service"
