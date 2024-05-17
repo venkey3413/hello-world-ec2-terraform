8ffd178c3c9659e78abb34f4a8686fce432885fa
AxAxHxAxAx <126902892+AxAxHxAxAx@users.noreply.github.com>
2024-05-17 20:57:33 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,45 +1,66 @@
 provider "aws" {
-  region = "us-east-1"  
+  region = "us-east-1"  # Replace with your desired region
 }
 
-resource "aws_ecs_cluster" "cluster" {
-  name = "hello-world-nodejs-cluster"
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = "my-ecs-cluster"
 }
 
-resource "aws_ecs_task_definition" "task_definition" {
-  family                   = "hello-world-nodejs-task"
-  network_mode             = "awsvpc"
+resource "aws_ecs_task_definition" "ecs_task_definition" {
+  family                   = "my-ecs-task"
   requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
   cpu                      = "256"
   memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
-  container_definitions = jsonencode([
-    {
-      name      = "hello-world-nodejs-container"
-      image     = "f86c6492a6bc"  # Update with your Docker image URL
-      cpu       = 256
-      memory    = 512
-      essential = true
-      portMappings = [
-        {
-          containerPort = 3000
-          hostPort      = 3000
-        }
-      ]
-    }
-  ])
+  container_definitions = jsonencode([{
+    name      = "my-node-app"
+    image     = var.docker_image
+    cpu       = 256
+    memory    = 512
+    essential = true
+    portMappings = [{
+      containerPort = 3000
+      hostPort      = 3000
+    }]
+  }])
 }
 
-resource "aws_ecs_service" "service" {
-  name            = "hello-world-nodejs-service"
-  cluster         = aws_ecs_cluster.cluster.arn
-  task_definition = aws_ecs_task_definition.task_definition.arn
+resource "aws_ecs_service" "ecs_service" {
+  name            = "my-ecs-service"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.ecs_task_definition.arn
   desired_count   = 1
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets         = ["subnet-05d87a98545cd0c56"]  # Update with your subnet ID
-    security_groups = ["sg-0561a79cc8203131b"]  # Update with your security group ID
+    subnets         = ["subnet-05d87a98545cd0c56]  # Replace with your subnet ID
+    security_groups = ["sg-0561a79cc8203131b"]      # Replace with your security group ID
     assign_public_ip = true
   }
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
+  ]
+}
+
+variable "docker_image" {
+  description = "Docker image name"
+  type        = string
 }
