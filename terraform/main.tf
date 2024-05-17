dc18eaf2611576a7d6b2345fa27e993f8c82b554
AxAxHxAxAx <126902892+AxAxHxAxAx@users.noreply.github.com>
2024-05-17 19:16:21 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,39 +2,21 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_ecs_cluster" "cluster" {
-  name = "hello-world-nodejs-cluster"
+resource "aws_ecr_repository" "my_repository" {
+  name = "my-repository"
 }
 
-variable "image_url" {}
-
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name               = "ecs-task-execution-role"
-  assume_role_policy = jsonencode({
-    "Version" => "2012-10-17",
-    "Statement" => [{
-      "Effect"    => "Allow",
-      "Principal" => {
-        "Service" => "ecs-tasks.amazonaws.com"
-      },
-      "Action"    => "sts:AssumeRole"
-    }]
-  })
+resource "aws_ecs_cluster" "my_cluster" {
+  name = "my-cluster"
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_attachment" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-resource "aws_ecs_task_definition" "task_definition" {
-  family                   = "hello-world-nodejs-task"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+resource "aws_ecs_task_definition" "my_task" {
+  family                   = "my-task"
   container_definitions    = <<DEFINITION
 [
   {
-    "name": "hello-world-nodejs",
-    "image": "${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}",
+    "name": "my-container",
+    "image": "${aws_ecr_repository.my_repository.repository_url}:latest",
     "essential": true,
     "portMappings": [
       {
@@ -47,17 +29,13 @@ resource "aws_ecs_task_definition" "task_definition" {
 DEFINITION
 }
 
-
-resource "aws_ecs_service" "service" {
-  name            = "hello-world-nodejs-service"
-  cluster         = aws_ecs_cluster.cluster.arn
-  task_definition = aws_ecs_task_definition.task_definition.arn
+resource "aws_ecs_service" "my_service" {
+  name            = "my-service"
+  cluster         = aws_ecs_cluster.my_cluster.id
+  task_definition = aws_ecs_task_definition.my_task.arn
   desired_count   = 1
-  launch_type     = "FARGATE"
+}
 
-  network_configuration {
-    subnets         = ["subnet-05d87a98545cd0c56"]  # Update with your subnet ID
-    security_groups = ["sg-0561a79cc8203131b"]  # Update with your security group ID
-    assign_public_ip = true
-  }
+output "url" {
+  value = aws_ecs_service.my_service.endpoint
 }
