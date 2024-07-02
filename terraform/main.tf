6eeb4ccd557b4ced342a86818db2e97c6d8d22f3
venkey3413 <144769456+venkey3413@users.noreply.github.com>
2024-07-02 18:46:08 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,53 +1,32 @@
 provider "aws" {
-  region = "us-east-1"  
+  region     = var.region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
-resource "aws_ecs_cluster" "ecs_cluster" {
-  name = "my-ecs-cluster"
-}
+resource "aws_instance" "nodejs_app" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
 
-data "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
-}
-
-resource "aws_ecs_task_definition" "ecs_task_definition" {
-  family                   = "my-ecs-task"
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
-  cpu                      = "256"
-  memory                   = "512"
-  execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
-
-  container_definitions = jsonencode([{
-    name      = "my-node-app"
-    image     = var.docker_image
-    cpu       = 256
-    memory    = 512
-    essential = true
-    portMappings = [{
-      containerPort = 3000
-      hostPort      = 3000
-    }]
-  }])
-}
-
-
-resource "aws_ecs_service" "ecs_service" {
-  name            = "my-ecs-service"
-  cluster         = aws_ecs_cluster.ecs_cluster.id
-  task_definition = aws_ecs_task_definition.ecs_task_definition.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    subnets         = ["subnet-05d87a98545cd0c56"]  
-    security_groups = ["sg-0561a79cc8203131b"]      
-    assign_public_ip = true
+  tags = {
+    Name = "nodejs-docker-app"
   }
-}
 
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update -y",
+      "sudo apt-get install -y docker.io",
+      "sudo systemctl start docker",
+      "sudo systemctl enable docker",
+      "sudo docker run -d -p 3000:3000 --name nodejs-app ${var.docker_image}"
+    ]
 
-variable "docker_image" {
-  description = "Docker image name"
-  type        = string
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file(var.key_path)
+      host        = self.public_ip
+    }
+  }
 }
